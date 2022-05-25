40d60d37da213348caa8b693370def021fe93a5f
Artem <sei.4okei@yandex.ru>
2022-05-25 17:17:54 +0300

    Сделал 6 страниц и переходы оп ним

---
+++ DemoExzT/DemoExzT/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DemoExzT.AppDateFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoExzT
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FrameObject.frMain = FrmMain;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FrmMain_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DemoExzT/DemoExzT/Pages/PageMain.xaml.cs
@@ -0,0 +1,34 @@
+﻿using DemoExzT.AppDateFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoExzT.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClient_Click(object sender, RoutedEventArgs e)
+        {
+            FrameObject.frMain.Navigate(new Pages.PageClient());
+        }
+    }
+}
